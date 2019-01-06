727e0a296270f1c7a57e11cec8c7ae42b1de3164
James Marquez <jmarquez@ualberta.ca>
2019-01-06 15:45:09 -0700

    Completed the program

--- ClassLoan.py
+++ ClassLoan.py
@@ -1,86 +1,89 @@
-import sys
+import math
 
-# Main Loan
-class Loan:
+
+class Loan:  # Main Loan
 
 	# Initializer / Instance Attributes
 	def __init__(self, amount, interest, age):
 		self.amount = amount  # Total Loan Amount
-		self.interest = interest / 100 
+		self.interest = interest / (12 * 100)
 		self.age = age  # Age should be in years
 		
 	# Loan Calculation Method
 	def loan_calculation(self):
 		
 		# Initial Variables for Loan Calculation
+		amount = self.amount
 		months = self.age * 12
-		principal_payment = self.amount / months
-		monthly_interest = self.interest / 12
+		monthly_interest = self.interest
+
+		# Formula Setup
+		numerator = monthly_interest * amount
+		denominator = 1 - ((1 + monthly_interest) ** (-1 * months))
+
+		calculated_monthly_payment = round(float(numerator/denominator), 2)
+
+		# Return Result for Method
+		return calculated_monthly_payment
 		
-		# Set Up for Monthly Loan Calculation
-		amount0 = self.amount
-		months_array = range(months)  # Array from 1-120
-		monthly_payments = [None] * months  # To be filled with required monthly payments
-		
-		# Monthly Loan Calculation
-		for i in months_array:
-			interest_payment = amount0 * monthly_interest
-			required_monthly_payment = interest_payment + principal_payment
-			monthly_payments[i] = required_monthly_payment
-			amount0 = amount0 - principal_payment
-			
-		calculated_monthly_payment = sum(monthly_payments) / months
-		result0 = "You must pay {} a month for {} years".format(calculated_monthly_payment, age)
-		
-		return result0
-		
-# Custom Loan
-class Custom_Loan(Loan):
-	def __init__(self, amount, interest, age, c_amount, c_years):
-		Loan.__init__(self, amount, interest, age)
+
+class CustomLoan(Loan):  # Custom Loan
+	def __init__(self, loan, c_amount, c_years):
+		super().__init__(loan.amount, loan.interest, loan.age)
 		self.c_amount = c_amount  # Custom Amount
 		self.c_years = c_years  # Custom Years
 		
-		if self.c_years > age:
-			print("Invalid amount of custom years")
-			sys.exit()
-		
-	def custom_calculation(self):
+	def custom_calculation(self, loan):
+
+		# Initial Variables for Custom Loan Calculation
 		c_months = self.c_years * 12
-		r_months = months - c_months
-		
-		c_months_array = range(c_months)
-		r_months_array = range(r_months)
-		c_monthly_payments = [None] * c_months
-		
-		amount1 = self.amount
-		
-		for i in c_months_array:
-			c_interest_payment = amount1 * monthly_interest
-			c_monthly_payment = c_interest_payment + self.c_amount
-			c_monthly_payments[i] = c_monthly_payment
-			amount1 = amount1 - self.c_amount
-			if amount1 <= 0:
-				completion = i + 1
-				string1 = "You can pay off your loans in {} months".format(completion)
-				result1 = print(string1)
-				break
-			
-		c_principal_payment = amount1 / r_months
-		r_monthly_payments = [None] * r_months
-				
-		for i in r_months_array:
-			r_interest_payment = c_principal_payment * monthly_interest
-			r_monthly_payment = r_interest_payment + c_principal_payment
-			r_monthly_payments[i] = r_monthly_payment
-			amount1 = amount1 - c_principal_payment
-				
-		new_monthly_payment = sum(r_monthly_payments) / r_months
-				
-		string2 = "If you pay {} for {} years, ".format(self.c_amount, self.c_years)
-		string3 = "you must pay {} for {} years.".format(new_monthly_payment, r_months / 12)
-		result1 = string2 + string3
-		
-		return result1
-						
-	
+		monthly_interest = loan.interest
+		amount = loan.amount
+
+		# Formula Setup from financeformulas.net/Remaining_Balance_Formula.html setting FV to FV
+		term_1 = amount * ((1 + monthly_interest) ** c_months)
+
+		term_2_numerator = self.c_amount * (((1 + monthly_interest) ** c_months) - 1)
+		term_2_denominator = monthly_interest
+
+		term_2 = term_2_numerator / term_2_denominator
+
+		term_12 = term_1 - term_2
+
+		# Amount after paying custom amount for custom period of time
+		remaining_amount = term_12
+
+		if remaining_amount > 0:
+			# Redeclare Variables
+			remaining_months = loan.age * 12 - c_months
+			new_numerator = ((1 + monthly_interest) ** remaining_months) - 1
+			new_denominator = monthly_interest * ((1 + monthly_interest) ** remaining_months)
+
+			# Re-adjust Formula
+			new_term = new_numerator / new_denominator
+
+			new_payment = round(remaining_amount / new_term, 2)
+
+			# Setup Result
+			remaining_years = round(remaining_months / 12, 2)
+			result1 = [remaining_years, new_payment]
+
+			# Return Result
+			return result1
+		else:
+			# Set Answer of Payments to 0 since loan is paid off before initial years
+			clean_payment = 0
+
+			# Reconfigure the Formula to Determine Number of Terms
+			clean_numerator = math.log(amount * monthly_interest / self.c_amount + 1)
+			clean_denominator = math.log(1 + monthly_interest)
+
+			# Solve the Number of Terms
+			number_of_terms_months = clean_numerator / clean_denominator
+			number_of_terms_years = round(number_of_terms_months / 12, 2)
+
+			# Setup Result
+			result1 = [number_of_terms_years, clean_payment]
+
+			# Return Result
+			return result1
